2202fb39e7aa8c43a5db221ce7e1290f97ee736c
shnpess <ann2lax@gmail.com>
2020-11-26 18:48:14 +0900

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Hello {
+
+    public static void main(String[] args) throws SQLException {
+        String hostname = "localhost";
+        String dbname = "<project_name>_development";
+        String username = "postgres";
+        String password = "pess01";
+        Connection conn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://" + hostname
+                    + ":5432/" + dbname, username, password);
+            System.out.println("接続成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
